93f7626af63ff5b7b06e4a328b4730dc87a2c32b
jqpeterson <jpeterson@aacs-us.com>
2015-11-06 18:04:20 -0600

    Finally getting data from the modem.

--- src/Protocol/ROC.hs
+++ src/Protocol/ROC.hs
@@ -1,10 +1,12 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE RankNTypes, OverloadedStrings #-}
 module Protocol.ROC where
-
+import Control.Monad
 import System.Hardware.Serialport
 import Conduit                     (($$),sourceLazy)
 import CRC16.Calculator
+import Data.Bimap                  (Bimap,empty,insert)
 import Data.Binary                 (get,put,decode,encode,Binary,Get)
 import Data.Binary.Get             (getLazyByteString,getWord8)
 import Data.Binary.Put             (putByteString,putLazyByteString,putWord8,runPut)
@@ -12,8 +14,9 @@ import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LB
 import Data.Conduit.Serialization.Binary  (sinkGet)
 import Data.Conduit.Network        (appSink,appSource,clientSettings,runTCPClient)
-import Data.List 
-import Data.IntMap.Strict          (minViewWithKey,size,Key)
+import Data.List                   ()
+import Data.IntMap.Strict          (fromList,minViewWithKey,singleton,size,Key,IntMap,foldrWithKey)
+import Data.Text                   (unpack,Text)
 import Data.Text.Encoding          (encodeUtf8)
 import Data.Word                   (Word8,Word16)
 import Roc.Protocol.Types
@@ -25,8 +28,8 @@ import Protocol.ROC.OpCodes
 import Protocol.ROC.RocSerialize
 import System.IO.Error             (tryIOError)
 
---import Numeric                                            
-
+import Numeric                                            
+import Debug.Trace
 
 -- | getPointType is designed for retrieving and entire PointType for serial comms only
 
@@ -59,28 +62,35 @@ import System.IO.Error             (tryIOError)
 
 -- | runOpCodeRaw is designed for running an opcode on the roc
   
--- runOpCodeRaw :: RocConfig -> (RocConfig -> BS.ByteString) -> IO BS.ByteString
--- runOpCodeRaw cfg opCode = do
---   let port = rocConfigPort cfg
---       commRate = rocCommSpeed cfg            
---   s <- openSerial port defaultSerialSettings { commSpeed = commRate }
---   _ <- send s $ BS.append (opCode cfg) (lzyBSto16BScrc.pack8to16 $ BS.unpack $ opCode cfg)
---   receivebs <- recvAllBytes s 255
---   closeSerial s
---   print $ showInt <$> BS.unpack receivebs <*> [""]
---   return receivebs
+runOpCodeRaw :: RocConfig -> (RocConfig -> BS.ByteString) -> IO BS.ByteString
+runOpCodeRaw cfg opCode = do
+  let port = rocConfigPort cfg
+      commRate = rocCommSpeed cfg  
+      bs = BS.append (opCode cfg) (lzyBSto16BScrc.pack8to16 $ BS.unpack $ opCode cfg)       
+  s <- openSerial port defaultSerialSettings { commSpeed = commRate }
+  _ <- send s $ bs
+  receivebs <- recvAllBytes s 255
+  closeSerial s
+  print $ showInt <$> BS.unpack bs <*> [""]
+  print $ showInt <$> BS.unpack receivebs <*> [""]
+  return receivebs
 
 
 requestOpCodeTableData :: HostAddress -> ModemConfig -> RocAccessConfig -> OpCodeTable l -> IO (Either String RocOpCodeResponse)
-requestOpCodeTableData hostAdd modemCfg accessCfg opTable = do    
-  let req = sourceLazy $ runPut.put $ buildOpTableRequest hostAdd accessCfg opTable
-      sendRequest server = do
-        req $$ appSink server
-        appSource server $$ sinkGet getRoutine
-  eitherRocOpCodeResponse <- tryIOError (runTCPClient (clientSettings (_port modemCfg) (encodeUtf8 $ _url modemCfg)) sendRequest)
-  case eitherRocOpCodeResponse of
-    Left err -> return.Left $ show err
-    Right resp -> return.Right $ resp
+requestOpCodeTableData hostAdd modemCfg accessCfg opTable =
+  case buildOpTableRequest hostAdd accessCfg opTable of
+    Left str -> return.Left $ str
+    Right (rocRequest:: RocOpCodeRequest) -> do
+      print $ showInt <$> BS.unpack (LB.toStrict.encode $ rocRequest) <*> [""]
+      print rocRequest
+      let req = sourceLazy $ runPut.put $ rocRequest
+          sendRequest server = do
+                          req $$ appSink server
+                          appSource server $$ sinkGet getRoutine
+      eitherRocOpCodeResponse <- tryIOError (runTCPClient (clientSettings (_port modemCfg) (encodeUtf8 $ _url modemCfg)) sendRequest)
+      case eitherRocOpCodeResponse of
+        Left err -> return.Left $ show err
+        Right resp -> return.Right $ resp
 
 buildOpTableRequest :: HostAddress -> RocAccessConfig -> OpCodeTable l -> Either String RocOpCodeRequest
 buildOpTableRequest hostAdd cfg table = (\ dataBytes ->  RocOpCodeRequest {reqRocAddress = RocAdd { rocUnitNumber = _unitNumber cfg, rocGroupNumber = _groupNumber cfg},
@@ -91,7 +101,7 @@ buildOpTableRequest hostAdd cfg table = (\ dataBytes ->  RocOpCodeRequest {reqRo
       maybeMinKey :: Maybe Word8
       maybeMinKey = fromIntegral . fst . fst <$> (minViewWithKey .  _unOpCodeData . _opCodeData $ table)
       numberOfKeys = fromIntegral . size . _unOpCodeData . _opCodeData $ table
-      eitherDataBytes = case (\ minKey ->  LB.pack [3, _unOpCodeTableId . _opCodeTableId $ table, minKey, numberOfKeys]) <$> maybeMinKey of
+      eitherDataBytes = case (\ minKey ->  LB.pack [ _unOpCodeTableId . _opCodeTableId $ table, minKey - 1 , numberOfKeys]) <$> maybeMinKey of
                           Nothing -> Left "No minkey found in OpCodeData IntMap"
                           Just bs -> Right bs
 
@@ -165,7 +175,7 @@ instance Binary RocOpCodeResponse where
       opCode            <- getWord8
       numberOfDataBytes <- getWord8
       dataByteString    <- getLazyByteString (2 + fromIntegral numberOfDataBytes)
-      let repackBytes = LB.pack [rocUnit,rocGroup,hostUnit,hostGroup,opCode,numberOfDataBytes] 
+      let repackBytes = LB.pack [hostUnit,hostGroup,rocUnit,rocGroup,opCode,numberOfDataBytes] 
       if checkCRC16 (ILazyBS $ LB.append repackBytes dataByteString) rocCRC16Config
       then return RocOpCodeResponse { respRocAddress = RocAdd { rocUnitNumber = rocUnit, rocGroupNumber = rocGroup},
                                       respHostAddress = HostAddress { hostUnitNumber = hostUnit, hostGroupNumber = hostGroup},
@@ -186,18 +196,89 @@ instance Binary RocOpCodeResponse where
 
 ----------------------------- Testing Stuff -----------------------------------------------
 
+
+testQuery :: IO (Either String RocOpCodeResponse)
+testQuery = do
+  doubleEither <- requestOpCodeTableData testHostAddress testModemConfig testRocAccessConfig `traverse` makeOpCodeTable
+  return $ join doubleEither
+
 testHostAddress :: HostAddress 
 testHostAddress = HostAddress 1 3
 
 testRocOpCodeRequest :: RocOpCodeRequest
-testRocOpCodeRequest = RocOpCodeRequest (RocAdd 240 240) (HostAddress 1 3) 10 $ LB.pack [0,0,3]
+testRocOpCodeRequest = RocOpCodeRequest (RocAdd 240 240) (HostAddress 1 3) 10 $ LB.pack [0,0,44]
 
 testRocOpCodeRequestByteString :: LazyByteString
 testRocOpCodeRequestByteString = "\240\240\SOH\ETX\n\ETX\NUL\NUL\ETX\195\247"
 
 testRocConfig :: RocConfig
 testRocConfig = RocConfig "/dev/ttyUSB0" [240,240] [1,3] CS19200 "LOI" 1000
- 
+
+testRocAccessConfig :: RocAccessConfig
+testRocAccessConfig = RocAccessConfig 240 240 "LOI" 1000
+
+testModemConfig :: ModemConfig
+testModemConfig = ModemConfig "166.131.38.15" 20090
+
+makeOpCodeTable :: Either String (OpCodeTable (TlpData ()))
+makeOpCodeTable = case (\ opTable -> OpCodeTable (OpCodeTableId 0) opTable makeBimap) <$> makeDataMap of
+                    Left txt -> Left $ unpack txt
+                    Right bs -> Right bs
+
+makeBimap :: Bimap Int OnPingId
+makeBimap = foldrWithKey bimapFcn empty myDataMap
+    where
+      bimapFcn k _ = insert k (OnPingId k)
+
+makeDataMap :: Either Text (OpCodeData (TlpData ()))
+makeDataMap = makeOpCodeData myDataMap
+
+myDataMap :: IntMap (TlpData ())
+myDataMap = fromList [( 1  , TlpUnsignedInt8 (Left ())),
+                      ( 2  , TlpUndefined32 (Left ())),
+                      ( 3  , TlpUnsignedInt8 (Left ())),
+                      ( 4  , TlpFloat (Left ())),
+                      ( 5  , TlpFloat (Left ())),
+                      ( 6  , TlpFloat (Left ())),
+                      ( 7  , TlpUnsignedInt8 (Left ())),
+                      ( 8  , TlpFloat (Left ())),
+                      ( 9  , TlpFloat (Left ())),
+                      ( 10 , TlpFloat (Left ())),
+                      ( 11 , TlpFloat (Left ())),
+                      ( 12 , TlpFloat (Left ())),
+                      ( 13 , TlpFloat (Left ())),
+                      ( 14 , TlpInt16 (Left ())),
+                      ( 15 , TlpInt16 (Left ())),
+                      ( 16 , TlpFloat (Left ())),
+                      ( 17 , TlpFloat (Left ())),
+                      ( 18 , TlpFloat (Left ())),
+                      ( 19 , TlpInt16 (Left ())),
+                      ( 20 , TlpFloat (Left ())),
+                      ( 21 , TlpInt16 (Left ())),
+                      ( 22 , TlpFloat (Left ())),
+                      ( 23 , TlpFloat (Left ())),
+                      ( 24 , TlpFloat (Left ())),
+                      ( 25 , TlpFloat (Left ())),
+                      ( 26 , TlpInt16 (Left ())),
+                      ( 27 , TlpFloat (Left ())),
+                      ( 28 , TlpFloat (Left ())),
+                      ( 29 , TlpFloat (Left ())),
+                      ( 30 , TlpFloat (Left ())),
+                      ( 31 , TlpFloat (Left ())),
+                      ( 32 , TlpFloat (Left ())),
+                      ( 33 , TlpFloat (Left ())), 
+                      ( 34 , TlpFloat (Left ())),
+                      ( 35 , TlpFloat (Left ())),
+                      ( 36 , TlpFloat (Left ())),
+                      ( 37 , TlpFloat (Left ())),
+                      ( 38 , TlpFloat (Left ())),
+                      ( 39 , TlpFloat (Left ())),
+                      ( 40 , TlpFloat (Left ())),
+                      ( 41 , TlpFloat (Left ())),
+                      ( 42 , TlpFloat (Left ())),
+                      ( 43 , TlpFloat (Left ())),
+                      ( 44 , TlpFloat (Left ()))]
+
 testingEncode :: IO ()
 testingEncode = do
     let manualByteString = BS.append (opCode10 testRocConfig) (lzyBSto16BScrc.pack8to16 $ BS.unpack $ opCode10 testRocConfig)
